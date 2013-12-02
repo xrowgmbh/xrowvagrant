abd7db0eaa3b8d0486b9f620ec4b7e76e0ae9aae
xrow <bjoern@xrow.de>
2013-12-02 15:56:44 +0100

    - update

--- Vagrantfile
+++ Vagrantfile
@@ -19,7 +19,7 @@ Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
 #   config.vm.network :forwarded_port, host: 10137, guest: 10137
 #   config.vm.network :forwarded_port, host: 20080, guest: 20080
     config.vm.synced_folder "C:\\workspace\\ezcluster", "/etc/ezcluster"
-    config.vm.synced_folder "C:\\workspace\\sites", "/var/www/sites"
+#    config.vm.synced_folder "C:\\workspace\\sites", "/var/www/sites"
 
     config.vm.provider "virtualbox" do |v|
         v.customize ["modifyvm", :id, "--memory", "2048"]
--- start.bat
+++ start.bat
@@ -1,13 +1,12 @@
 @echo off
 
+CALL vagrant destroy
 CALL rmdir /S /Q C:\Users\bjoernd\.vagrant.d\boxes\centos64
 CALL vagrant up
 rem 0.10 is not yet released so isntall from source https://github.com/dotless-de/vagrant-vbguest/issues/88
 CALL vagrant plugin install --plugin-source https://rubygems.org --plugin-prerelease vagrant-vbguest
-rem CALL vagrant halt
+CALL vagrant halt
 rem get contents .vagrant\machines\centos64\virtualbox\id   is 1325059c-dd53-41ab-b2d0-2cd2b002493d
 rem CALL vagrant package --base 1325059c-dd53-41ab-b2d0-2cd2b002493d --output ezpublish.box --vagrantfile Vagrantfile.dist
 pause
-rem qemu-img convert -O raw CloudBioLinux-32bit-disk1.vmdk CloudBioLinux-32bit-disk1.img
-rem s3cmd put --acl-public --guess-mime-type biolinux_20110122.box s3://chapmanb/biolinux_20110122.box
 
