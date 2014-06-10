6be5913581ef6bbee0d4f66465998cbbf646c00d
xrow <bjoern@xrow.de>
2014-06-10 16:12:18 +0200

    - fix install symfony.sh

--- ezcluster/scripts/symfony.sh
+++ ezcluster/scripts/symfony.sh
@@ -3,7 +3,7 @@
 sudo rm -Rf * .gitignore .travis.yml
 composer -vvv create-project symfony/framework-standard-edition . 2.5.* --prefer-dist --no-interaction
 
-cat <<EOL > app/config/parameters2.yml
+cat <<EOL > app/config/parameters.yml
 # This file is auto-generated during the composer install
 parameters:
     database_driver: pdo_mysql
@@ -18,20 +18,20 @@ parameters:
     mailer_password: null
     locale: en
     secret: c60211546d9737d16d4a34e90fb9f7b1d69bacba
+    debug_toolbar: true,
+    debug_redirects: false,
+    use_assetic_controller: true,
     database_path: null
 EOL
 
-cp app/config/parameters2.yml app/config/parameters2.yml.dist
-#mv web/config.php web/config.php.dist
-#mv web/app.php web/index.php
-#mv web/app_dev.php web/index_dev.php
+cp app/config/parameters.yml app/config/parameters.yml.dist
+mv web/config.php web/config.php.dist
 
 composer -vvv install --no-interaction
 composer -vvv update --no-interaction
 sed -i "s/\/\/umask(/umask(/g" app/console
 sed -i "s/\/\/umask(/umask(/g" web/app_dev.php
 sed -i "s/\/\/umask(/umask(/g" web/app.php
-#sed -i "s/app/index/g" web/.htaccess
 
 find app/{cache,logs,config} -type d | sudo xargs chmod -R 777
 find app/{cache,logs,config} -type f | sudo xargs chmod -R 666
