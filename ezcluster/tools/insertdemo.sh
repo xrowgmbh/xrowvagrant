f615577d130fbaf2c5a9da83541b5f66b5d5335d
xrow <bjoern@xrow.de>
2014-06-13 16:06:00 +0200

    - update

--- ezcluster/tools/insertdemo.sh
+++ ezcluster/tools/insertdemo.sh
@@ -2,9 +2,10 @@
 PWD=$(pwd)
 rm -Rf ezpublish_legacy/settings/override ezpublish_legacy/settings/siteaccess ezpublish_legacy/var/ezdemo_site/log ezpublish_legacy/var/ezdemo_site/cache
 wget --no-check-certificate -O dump.tgz https://raw.github.com/xrowgmbh/xrowvagrant/master/ezcluster/demo/dump.tgz
-tar --no-same-permissions -xzvf dump-5.3.tgz
+tar --no-same-permissions -xzvf dump.tgz
 rm -Rf ezpublish_legacy/var/ezdemo_site/log/ ezpublish_legacy/var/ezdemo_site/cache/
-mysql -e'create database ezpublish'
-mysql ezpublish < ezpublish_legacy/var/ezdemo_site/dump.sql
-rm -Rf dump-5.3.tgz
+mysql -uroot -e'create database ezpublish'
+mysql -uroot ezpublish < ezpublish_legacy/var/ezdemo_site/dump.sql
+rm -Rf dump.tgz
+php ezpublish/console ezpublish:legacy:script bin/php/ezpgenerateautoloads.php
 source ./fixpermissions.sh
