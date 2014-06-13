69e5baf9e83afd0457d4660a33b47264521f92ab
xrow <bjoern@xrow.de>
2014-06-13 16:56:28 +0200

    - update

--- ezcluster/tools/dumpdemo.sh
+++ ezcluster/tools/dumpdemo.sh
@@ -4,4 +4,4 @@ source ./cache.sh
 mysqldump -n --default-character-set=utf8 --opt --single-transaction -hlocalhost -uroot ezpublish > ezpublish_legacy/var/ezdemo_site/dump.sql
 sudo mkdir -p /etc/ezcluster/demo/
 rm -Rf ezpublish_legacy/var/ezdemo_site/log/ ezpublish_legacy/var/ezdemo_site/cache/
-sudo tar -czf /etc/ezcluster/demo/dump.tgz -C $PWD ./ezpublish_legacy/settings/override ./ezpublish_legacy/settings/siteaccess/ ./ezpublish_legacy/var/ezdemo_site ./ezpublish/config/
+sudo tar -czf /etc/ezcluster/demo/dump.tgz -C $PWD ./ezpublish_legacy/var/autoload settings/override ./ezpublish_legacy/settings/override ./ezpublish_legacy/settings/siteaccess/ ./ezpublish_legacy/var/ezdemo_site ./ezpublish/config/
