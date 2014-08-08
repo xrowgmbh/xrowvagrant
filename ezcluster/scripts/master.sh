95ba52e94fd5fef3b2d8561568afd24f9a999b5f
xrow <bjoern@xrow.de>
2014-08-08 17:37:44 +0200

    - update

--- ezcluster/scripts/master.sh
+++ ezcluster/scripts/master.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
-# broken since php 5.4 requirement
 rm -Rf * .git .gitignore .travis.yml
 git clone --depth 1 https://github.com/ezsystems/ezpublish-community.git .
 git clone --depth 1 https://github.com/ezsystems/ezpublish-legacy.git ezpublish_legacy
@@ -12,6 +11,7 @@ git clone --depth 1 https://github.com/ezsystems/ezfind ezpublish_legacy/extensi
 #git clone https://github.com/xrowgmbh/xrowsass ezpublish_legacy/extension/xrowsass
 #git clone https://github.com/xrowgmbh/xrowgis ezpublish_legacy/extension/xrowgis
 #git clone https://github.com/xrowgmbh/xrowsearch ezpublish_legacy/extension/xrowsearch
+#git clone https://github.com/xrowgmbh/xrowmetadata ezpublish_legacy/extension/xrowmetadata
 #git clone https://github.com/xrowgmbh/xrowmetadata ezpublish_legacy/extension/xrowmetadata
 
 sed -i "/^\[RepositorySettings\]/,/^\[/ {
@@ -27,9 +27,19 @@ umask(0000);' web/index.php
 find {ezpublish/{cache,logs,config,sessions},ezpublish_legacy/{design,extension,settings,var},web} -type d | xargs chmod -R 777
 find {ezpublish/{cache,logs,config,sessions},ezpublish_legacy/{design,extension,settings,var},web} -type f | xargs chmod -R 666
 
-composer update
+RANDOM=´openssl rand -base64 32´
+cat <<EOL > ./ezpublish/config/parameters.yml
+# This file is auto-generated during the composer install
+parameters:
+    secret: $RANDOM
+    locale_fallback: en
+    ezpublish_legacy.default.view_default_layout: 'eZDemoBundle::pagelayout.html.twig'
+EOL
 
-php vendor/sensio/distribution-bundle/Sensio/Bundle/DistributionBundle/Resources/bin/build_bootstrap.php ezpublish
+composer require ezsystems/platform-ui-bundle:dev-master
+
+#composer update
+#php vendor/sensio/distribution-bundle/Sensio/Bundle/DistributionBundle/Resources/bin/build_bootstrap.php ezpublish
 
 php ezpublish/console assets:install --symlink --relative web
 php ezpublish/console ezpublish:legacy:assets_install --symlink web
@@ -39,5 +49,5 @@ wget -O web/robots.txt http://s3-eu-west-1.amazonaws.com/xrow/downloads/ezcluste
 wget -O web/.htaccess http://s3-eu-west-1.amazonaws.com/xrow/downloads/ezcluster/.htaccess
 
 cp -a /etc/ezcluster/tools/* .
-source ./insertdemo.sh
+#source ./insertdemo.sh
 source ./cache.sh
